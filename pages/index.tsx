e5ea0a5a2a1b6fc4c70768623cc12fb3a6cb9512
Faraz Patankar <farazpatankar@gmail.com>
2021-06-03 19:41:22 +0400

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,22 +35,6 @@ export const Home = (): JSX.Element => (
           <h3>Learn &rarr;</h3>
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
       </div>
     </main>
 
